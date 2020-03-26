af942a72d74e54d5cdc843509985800ec13b58b5
Vladimir Pershukov <vladimirpershukov@yandex.ru>
2020-03-26 18:35:33 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# visionlabs_test_assignment
-Test assignment for VisionLabs 
+1.install
+sudo pip3 install Flask
+
+2.run
+python3 test_mock.py
+
+3.urls
+'/'
+'/user/<username>'
+'/post/<int: post id>'
+
+4.write tests on python to check these urls.
---
+++ test_mock.py
@@ -0,0 +1,28 @@
+#! /usr/bin/env python
+from flask import Flask, json, jsonify
+import argparse
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({"key": "value"})
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return jsonify({"User": username})
+
+
+@app.route('/post/<int:post_id>', methods=['GET'])
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return jsonify({"post" : post_id})
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='port')
+    parser.add_argument('--port', dest='port', action='store', type=int, help='using port for mock', default=6000)
+    args = parser.parse_args()
+    app.run(host='0.0.0.0', port=args.port, debug=True, threaded=True)
+
